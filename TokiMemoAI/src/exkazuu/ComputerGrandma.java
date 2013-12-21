566fe08f06fa757a9af0c89d54b616a2f836161c
Kazunori SAKAMOTO <exkazuu@gmail.com>
2013-12-21 15:29:29 +0900

    Writing my AI program.

---
+++ TokiMemoAI/src/exkazuu/ComputerGrandma.java
@@ -0,0 +1,19 @@
+package exkazuu;
+
+import engine.InternalPlayer;
+import engine.api.Computer;
+
+public class ComputerGrandma implements Computer {
+
+  public int[] createParameters() {
+    return null;
+  }
+
+  @Override
+  public int doTurn(int selfId, InternalPlayer[] players, int[] mostFavoritePlayers) {
+
+
+
+    return 0;
+  }
+}
