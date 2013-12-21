5fdd25d5192f5263f006870464ee13dec796d2e3
ss0201 <ss0201.777@gmail.com>
2013-12-21 15:44:49 +0900

    Engine completed

--- TokiMemoAI/src/engine/GameEngine.java
+++ TokiMemoAI/src/engine/GameEngine.java
@@ -3,41 +3,44 @@ package engine;
 import java.util.ArrayList;
 
 import engine.api.Computer;
+import engine.api.Player;
 
 public class GameEngine {
-	private ArrayList<InternalPlayer> internalPlayers;
-	private ArrayList<Heroine> heroines;
-	private ArrayList<Computer> computers;
+  private ArrayList<InternalPlayer> internalPlayers;
+  private ArrayList<Heroine> heroines;
+  private ArrayList<Computer> computers;
 
-	public GameEngine() {
-		internalPlayers = new ArrayList<InternalPlayer>();
-		heroines = new ArrayList<Heroine>();
-		for (int i = 0; i < 4; i++) {
-			heroines.add(new Heroine());
-		}
-	}
+  public GameEngine() {
+    internalPlayers = new ArrayList<InternalPlayer>();
+    heroines = new ArrayList<Heroine>();
+    for (int i = 0; i < 4; i++) {
+      heroines.add(new Heroine());
+    }
+  }
 
-	public void proceed() {
-		ArrayList<Integer> mostFavoritePlayers;
-		for (Heroine heroine : heroines) {
-			int bestPlayer = 0;
-			int bestEvaluation = 0;
-			for (int i = 0; i < internalPlayers.size(); i++) {
-				int evaluation = heroine.getEvaluation(internalPlayers.get(i));
-				if (bestEvaluation < heroine.getEvaluation(internalPlayers
-						.get(i))) {
-					bestPlayer = i;
-					bestEvaluation = evaluation;
-				}
-			}
-		}
+  public void proceed() {
+    ArrayList<Integer> mostFavoritePlayers = new ArrayList<Integer>();
+    for (Heroine heroine : heroines) {
+      int bestPlayer = 0;
+      int bestEvaluation = 0;
+      for (int i = 0; i < internalPlayers.size(); i++) {
+        int evaluation = heroine.getEvaluation(internalPlayers.get(i));
+        if (bestEvaluation < heroine.getEvaluation(internalPlayers.get(i))) {
+          bestPlayer = i;
+          bestEvaluation = evaluation;
+        }
+      }
+      mostFavoritePlayers.add(bestPlayer);
+    }
 
-		for (int i = 0; i < computers.size(); i++) {
-			//computer.doTurn(i, internalPlayers, )
-		}
+    ArrayList<Player> players = new ArrayList<Player>();
+    for (Player player : internalPlayers) {
+      players.add(player);
+    }
 
-		// for (InternalPlayer player : internalPlayers) {
-		//
-		// }
-	}
+    for (int i = 0; i < computers.size(); i++) {
+      int index = computers.get(i).doTurn(i, players, mostFavoritePlayers);
+      internalPlayers.get(i).addParameter(index);
+    }
+  }
 }
