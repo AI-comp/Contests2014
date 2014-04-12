c8b39f74444fb7e4b13e7e71f7d3d960a5922db1
ss <ss0201.777@gmail.com>
2014-04-12 17:20:00 +0900

    Delete obsolete engine

--- TokiMemoAI/src/engine/api/Computer.java
+++
@@ -1,8 +0,0 @@
-package engine.api;
-
-import java.util.List;
-import java.util.Map.Entry;
-
-public interface Computer {
-  Entry<Player.Command, Integer> doTurn(int selfId, List<Player> players, List<Heroine> heroines);
-}
